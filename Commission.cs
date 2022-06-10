681927efcdcf60b6e96fcfceffd8bf2fee82d3be
Bryan Williams <bryan.williams@hrblock.com>
2022-06-10 14:35:20 -0600

    Changes

---
+++ Commission.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesCommissionCalculator
+{
+    class Commission
+    {
+        private const decimal ITEM1 = 239.9M;
+        private const decimal ITEM2 = 129.75M;
+        private const decimal ITEM3 = 99.95M;
+        private const decimal ITEM4 = 350.89M;
+        private const decimal WEEKLY_PAY = 200.00M;
+        private const decimal PERCENTAGE = 0.09M;
+  
+
+        public Commission(int item1Sold, int item2Sold, int item3Sold, int item4Sold)
+        {
+            Item1Sold = item1Sold;
+            Item2Sold = item1Sold;
+            Item3Sold = item1Sold;
+            Item4Sold = item1Sold;
+        }
+
+        public int Item1Sold { get; set; }
+        public int Item2Sold { get; set; }
+        public int Item3Sold { get; set; }
+        public int Item4Sold { get; set; }
+
+        public decimal GetTotalSales()
+        {
+            return ITEM1 * Item1Sold + ITEM2 * Item2Sold + ITEM3 * Item3Sold + ITEM4 * Item4Sold;
+        }
+
+        public decimal CommissionRate()
+        {
+            return GetTotalSales() * PERCENTAGE + WEEKLY_PAY;
+        }
+    }
+}
---
+++ SalesCommissonCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SalesCommissionCalculator
+{
+    class SalesCommisionCalculator
+    {
+        static void Main(string[] args)
+        {
+            int item1Sold = 0;
+            int item2Sold = 0;
+            int item3Sold = 0;
+            int item4Sold = 0;
+
+            Console.WriteLine("Sales Commission Calculator");
+            Console.WriteLine("============================");
+
+
+            //input-input
+            Console.WriteLine("Enter total of item 1 sold");
+            item1Sold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter total of item 2 sold");
+            item2Sold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter total of item 3 sold");
+            item3Sold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter total of item 4 sold");
+            item4Sold = Convert.ToInt32(Console.ReadLine());
+
+            Commission cs = new Commission(item1Sold, item2Sold, item3Sold, item4Sold);
+            Console.WriteLine("============================");
+            Console.WriteLine("");
+
+            //total sales
+            Console.WriteLine($"Total Items Sold for the week = {item1Sold + item2Sold + item3Sold + item4Sold}");
+            Console.WriteLine("Total Sales for the week = {0:C}", cs.GetTotalSales());
+            Console.WriteLine("");
+            Console.WriteLine("===============================");
+            Console.WriteLine("");
+
+            //total earnings
+            Console.WriteLine("Total Earnings (Weekly Pay ($200) + Commission)  = {0:C}", cs.CommissionRate());
+            Console.ReadLine();
+        }
+
+    }
+}
